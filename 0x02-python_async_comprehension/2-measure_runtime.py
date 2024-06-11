3f9f6ceefc2123a96d8041af43da6fd3655b5288
Larst7 <lasgwenzi7@gmail.com>
2024-06-11 20:57:53 +0200

    Add measure_runtime to run async comprehensions in parallel and measure total time

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
-"""
-This module contains a function to measure the runtime of
-executing async_comprehension four times in parallel.
-"""
+""" Module that runs time for four parallel comprehensions"""
 
 import asyncio
 import time
-from .1_async_comprehension import async_comprehension
+from importlib import import_module as using
+
+
+async_comprehension = using('1-async_comprehension').async_comprehension
+
 
 async def measure_runtime() -> float:
-    """
-    Measures the runtime of running async_comprehension four times in parallel.
-
-    Returns:
-        float: The total runtime for executing the comprehensions in seconds.
-    """
-    start_time = time.perf_counter()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    end_time = time.perf_counter()
-    return end_time - start_time
+    """displays function """
+    start = time.time()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    end = time.time()
+    return end - start
